8e67c55e85064637f97bdd21df02933a402c5aa4
james.wood <jwooddev@gmail.com>
2023-01-27 16:25:18 -0600

    Started working on destructuring a given structure into an array of objects with their paths so that I can rebuild it up with the appropriate json paths.

--- unflatten.go
+++ unflatten.go
@@ -1,8 +1,86 @@
 package flat
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
 
-// TODO: Implement Unflatten
-func Unflatten(item any) ([]byte, error) {
+type Unflattener struct {
+	delimiter string
+}
+
+func NewUnflattener(delimiter string) *Unflattener {
+	return &Unflattener{delimiter: delimiter}
+}
+
+func (u *Unflattener) Delimiter(delimiter string) {
+	u.delimiter = delimiter
+}
+
+// TODO: Implement unflatten
+func (u *Unflattener) Unflatten(item any) ([]byte, error) {
+	if reflect.TypeOf(item).Kind() != reflect.Struct {
+		return nil, errors.New("unflatten expects a struct as input")
+	}
+
 	return json.Marshal(item)
+}
+
+type meta struct {
+	value reflect.Value
+	field reflect.StructField
+	path  []string
+}
+
+// fetchMeta is a recursive function that generates a list of metadata objects for each field in the struct
+func (u *Unflattener) fetchMeta(item any, currentPath []string) (metadata []meta, err error) {
+	if reflect.TypeOf(item).Kind() != reflect.Struct {
+		errstr := fmt.Sprintf("cannot get metadata from a non-struct (%s)", strings.Join(currentPath, u.delimiter))
+		return nil, errors.New(errstr)
+	}
+
+	ValueOf := reflect.ValueOf(item)
+
+	fields := ValueOf.Type().NumField()
+	for i := 0; i < fields; i++ {
+		field := ValueOf.Type().Field(i)
+
+		// Get the json path from the json struct tag
+		var JsonPath string
+		{
+			tag, ok := field.Tag.Lookup("json")
+			if ok {
+				split := strings.Split(tag, ",")
+				JsonPath = split[0]
+			}
+
+			if JsonPath == "" {
+				JsonPath = field.Name
+			}
+		}
+
+		// If the field is a struct, recursively call fetchMeta
+		// Otherwise, append the metadata to the list
+		SplitJsonPath := strings.Split(JsonPath, u.delimiter)
+		if field.Type.Kind() == reflect.Struct {
+			subPath := append(currentPath, SplitJsonPath...)
+			subMetadata, err := u.fetchMeta(ValueOf.Field(i).Interface(), subPath)
+			if err != nil {
+				return nil, err
+			}
+
+			metadata = append(metadata, subMetadata...)
+		} else {
+			metadata = append(metadata, meta{
+				value: ValueOf.Field(i),
+				field: field,
+				path:  append(currentPath, SplitJsonPath...),
+			})
+		}
+	}
+
+	return
 }
--- unflatten_test.go
+++ unflatten_test.go
@@ -1,18 +1,55 @@
 package flat
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestUnflatten(t *testing.T) {
 	type test struct {
 		Value string `json:"inner.value"`
 	}
 
-	data, err := Unflatten(test{Value: "hello"})
+	u := NewUnflattener(".")
+
+	data, err := u.Unflatten(test{Value: "hello"})
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if string(data) != `{"inner":{"value":"hello"}}` {
 		t.Fatal("unflatten failed. Expected: {\"inner\":{\"value\":\"hello\"}}. Got: ", string(data))
+	}
+}
+
+func TestFetchMetaSimple(t *testing.T) {
+	type test struct {
+		IKey   string `json:"inner.key"`
+		IValue string `json:"inner.value"`
+		OKey   string `json:"outer.key"`
+		OValue string `json:"outer.value"`
+	}
+
+	u := NewUnflattener(".")
+
+	metadata, err := u.fetchMeta(test{"hello", "world", "hello", "world"}, []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(metadata) != 4 {
+		t.Fatal("fetchMeta failed. Expected 1 metadata object. Got: ", len(metadata))
+	}
+
+	if metadata[0].path[0] != "inner" && metadata[0].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[1].path[0] != "inner" && metadata[1].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[2].path[0] != "outer" && metadata[3].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[3].path[0] != "outer" && metadata[3].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
 	}
 }
