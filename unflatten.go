69322f1afa4c9ab3ac042aef6a94740d19a897d6
james.wood <jwooddev@gmail.com>
2023-01-28 08:48:44 -0600

    Allow for pointers and fields that are pointers.

--- unflatten.go
+++ unflatten.go
@@ -22,8 +22,14 @@ func (u *Unflattener) Delimiter(delimiter string) {
 
 // TODO: Implement unflatten
 func (u *Unflattener) Unflatten(item any) ([]byte, error) {
-	if reflect.TypeOf(item).Kind() != reflect.Struct {
-		return nil, errors.New("unflatten expects a struct as input")
+	TypeOf := reflect.TypeOf(item)
+	if TypeOf.Kind() != reflect.Struct && !(TypeOf.Kind() == reflect.Ptr && TypeOf.Elem().Kind() == reflect.Struct) {
+		return nil, errors.New("unflatten expects a struct or pointer to struct as input")
+	}
+
+	// If it's a pointer, dereference it
+	if TypeOf.Kind() == reflect.Ptr {
+		item = reflect.ValueOf(item).Elem().Interface()
 	}
 
 	return json.Marshal(item)
@@ -37,9 +43,15 @@ type meta struct {
 
 // fetchMeta is a recursive function that generates a list of metadata objects for each field in the struct
 func (u *Unflattener) fetchMeta(item any, currentPath []string) (metadata []meta, err error) {
-	if reflect.TypeOf(item).Kind() != reflect.Struct {
-		errstr := fmt.Sprintf("cannot get metadata from a non-struct (%s)", strings.Join(currentPath, u.delimiter))
+	TypeOf := reflect.TypeOf(item)
+	if TypeOf.Kind() != reflect.Struct && !(TypeOf.Kind() == reflect.Ptr && TypeOf.Elem().Kind() == reflect.Struct) {
+		errstr := fmt.Sprintf("cannot get metadata from a non-struct or non-pointer-to-struct (%s)", strings.Join(currentPath, u.delimiter))
 		return nil, errors.New(errstr)
+	}
+
+	// If it's a pointer, dereference it
+	if TypeOf.Kind() == reflect.Ptr {
+		item = reflect.ValueOf(item).Elem().Interface()
 	}
 
 	ValueOf := reflect.ValueOf(item)
@@ -65,7 +77,8 @@ func (u *Unflattener) fetchMeta(item any, currentPath []string) (metadata []meta
 		// If the field is a struct, recursively call fetchMeta
 		// Otherwise, append the metadata to the list
 		SplitJsonPath := strings.Split(JsonPath, u.delimiter)
-		if field.Type.Kind() == reflect.Struct {
+		if field.Type.Kind() == reflect.Struct ||
+			(field.Type.Kind() == reflect.Ptr && field.Type.Elem().Kind() == reflect.Struct) {
 			subPath := append(currentPath, SplitJsonPath...)
 			subMetadata, err := u.fetchMeta(ValueOf.Field(i).Interface(), subPath)
 			if err != nil {
--- unflatten_test.go
+++ unflatten_test.go
@@ -37,19 +37,134 @@ func TestFetchMetaSimple(t *testing.T) {
 	}
 
 	if len(metadata) != 4 {
-		t.Fatal("fetchMeta failed. Expected 1 metadata object. Got: ", len(metadata))
+		t.Fatal("fetchMeta failed. Expected 4 metadata object. Got: ", len(metadata))
 	}
 
-	if metadata[0].path[0] != "inner" && metadata[0].path[1] != "value" {
+	if metadata[0].path[0] != "inner" || metadata[0].path[1] != "key" {
 		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
 	}
-	if metadata[1].path[0] != "inner" && metadata[1].path[1] != "key" {
+	if metadata[1].path[0] != "inner" || metadata[1].path[1] != "value" {
 		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
 	}
-	if metadata[2].path[0] != "outer" && metadata[3].path[1] != "value" {
+	if metadata[2].path[0] != "outer" || metadata[2].path[1] != "key" {
 		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
 	}
-	if metadata[3].path[0] != "outer" && metadata[3].path[1] != "key" {
+	if metadata[3].path[0] != "outer" || metadata[3].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+}
+
+func TestFetchMetaInnerStruct(t *testing.T) {
+	type inner struct {
+		IKey   string `json:"key"`
+		IValue string `json:"value"`
+	}
+	type test struct {
+		Inner  inner  `json:"inner"`
+		OKey   string `json:"outer.key"`
+		OValue string `json:"outer.value"`
+	}
+
+	u := NewUnflattener(".")
+
+	metadata, err := u.fetchMeta(test{inner{"hello", "world"}, "hello", "world"}, []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(metadata) != 4 {
+		t.Fatal("fetchMeta failed. Expected 4 metadata object. Got: ", len(metadata))
+	}
+
+	if metadata[0].path[0] != "inner" || metadata[0].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[1].path[0] != "inner" || metadata[1].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[2].path[0] != "outer" || metadata[2].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[3].path[0] != "outer" || metadata[3].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+}
+
+func TestFetchMetaInnerStructPointer(t *testing.T) {
+	type inner struct {
+		IKey   string `json:"key"`
+		IValue string `json:"value"`
+	}
+	type test struct {
+		Inner  *inner `json:"inner"`
+		OKey   string `json:"outer.key"`
+		OValue string `json:"outer.value"`
+	}
+
+	u := NewUnflattener(".")
+
+	metadata, err := u.fetchMeta(test{&inner{"hello", "world"}, "hello", "world"}, []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(metadata) != 4 {
+		t.Fatal("fetchMeta failed. Expected 4 metadata object. Got: ", len(metadata))
+	}
+
+	if metadata[0].path[0] != "inner" || metadata[0].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[1].path[0] != "inner" || metadata[1].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[2].path[0] != "outer" || metadata[2].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[3].path[0] != "outer" || metadata[3].path[1] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+}
+
+func TestFetchMetaInnerNestedStructAndPointer(t *testing.T) {
+	type value struct {
+		Test1 string `json:"value"`
+		Test2 string `json:"value2"`
+	}
+	type inner struct {
+		IKey   string `json:"key"`
+		IValue *value `json:"value"`
+	}
+	type test struct {
+		Inner  inner  `json:"inner"`
+		OKey   string `json:"outer.key"`
+		OValue string `json:"outer.value"`
+	}
+
+	u := NewUnflattener(".")
+
+	metadata, err := u.fetchMeta(test{inner{"hello", &value{"world1", "world2"}}, "hello", "world"}, []string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(metadata) != 5 {
+		t.Fatal("fetchMeta failed. Expected 5 metadata object. Got: ", len(metadata))
+	}
+
+	if metadata[0].path[0] != "inner" || metadata[0].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[1].path[0] != "inner" || metadata[1].path[1] != "value" || metadata[1].path[2] != "value" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[2].path[0] != "inner" || metadata[2].path[1] != "value" || metadata[2].path[2] != "value2" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[3].path[0] != "outer" || metadata[3].path[1] != "key" {
+		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
+	}
+	if metadata[4].path[0] != "outer" || metadata[4].path[1] != "value" {
 		t.Fatal("fetchMeta failed. Expected path to be 'inner.value'. Got: ", metadata[0].path)
 	}
 }
